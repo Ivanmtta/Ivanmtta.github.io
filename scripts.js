f0fce54885a163078f881ae6ef3a8ef3e3d5bd52
Ivan <ivanmtta@gmail.com>
2018-06-03 03:49:00 -0600

    minor improvements

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function hover(element){
+	element.setAttribute('src', "./img/placeholderAnimated.gif");
+}
+
+function unhover(element){
+	element.setAttribute('src', "./img/placeholderstatic.png");
+}
