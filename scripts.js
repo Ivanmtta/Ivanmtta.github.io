48b841b8646faaf8810957a73046ffca96d6bf65
Ivan <ivanmtta@gmail.com>
2018-06-03 13:15:16 -0600

    Added graphics

--- scripts.js
+++ scripts.js
@@ -5,3 +5,27 @@ function hover(element){
 function unhover(element){
 	element.setAttribute('src', "./img/placeholderstatic.png");
 }
+
+function hoverMoonLander(element){
+	element.setAttribute('src', "./img/moonlanderanimated.gif");
+}
+
+function unhoverMoonLander(element){
+	element.setAttribute('src', "./img/moonlanderstatic.png");
+}
+
+function hoverspacescape(element){
+	element.setAttribute('src', "./img/spacescapeanimated.gif");
+}
+
+function unhoverspacescape(element){
+	element.setAttribute('src', "./img/spacescapestatic.png");
+}
+
+function catrunnerhover(element){
+	element.setAttribute('src', "./img/catrunneranimated.gif");
+}
+
+function catrunnerunhover(element){
+	element.setAttribute('src', "./img/catrunnerstatic.png");
+}
